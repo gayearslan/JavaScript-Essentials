d3428d32668996f60a8c6f7ddb981720030f82ee
Gaye Arslan <gaye_arslan@hotmail.com>
2025-10-25 17:34:35 +0300

    elemt uzerinde gezme

---
+++ Elementler üzerinde gezinme/app.js
@@ -0,0 +1,11 @@
+let value;
+
+// console.log(document.location);
+// value = document.location.hostname;
+
+//value = document.characterSet;
+//value = document.title;
+//value=document.links;
+value=document.designMode;
+
+console.log(value);
