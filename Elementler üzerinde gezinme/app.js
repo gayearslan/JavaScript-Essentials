9ca3065237f7f986c9d1feeb2ef5c6d13db9070d
Gaye Arslan <gaye_arslan@hotmail.com>
2025-10-25 18:23:06 +0300

    guncellendi

--- Elementler üzerinde gezinme/app.js
+++ Elementler üzerinde gezinme/app.js
@@ -1,11 +1,35 @@
+// let value;
+
+ // console.log(document.location);
+ // value = document.location.hostname;
+
+ //value = document.characterSet;
+ //value = document.title;
+ //value=document.links;
+ //value=document.designMode;
+ //value= document.links.item(4);
+// console.log(value);
+
+const todo = document.querySelector(".list-group-item");
+const todolist = document.querySelector(".list-group");
+const card = document.querySelector(".card");
+
+
 let value;
 
-// console.log(document.location);
-// value = document.location.hostname;
+//anneden çocuklara erişmek
+// value = todolist.children[0];
+// value = todolist.children[1];
 
-//value = document.characterSet;
-//value = document.title;
-//value=document.links;
-value=document.designMode;
+
+// value = Array.from(todolist.children);
+
+// value.forEach(function(todo){
+//     console.log(todo.textContent);
+// })
+
+
+value = todo;
+value = todo.parentElement.parentElement;
 
 console.log(value);
